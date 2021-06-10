6f9f90ceb7798e7df7dc9c798f13a9f2fdcd509d
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-10 23:01:50 +0200

    Create Application

---
+++ src/main/java/com/github/alepergola/spring_framework_in_depth_2/Application.java
@@ -0,0 +1,9 @@
+package com.github.alepergola.spring_framework_in_depth_2;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
