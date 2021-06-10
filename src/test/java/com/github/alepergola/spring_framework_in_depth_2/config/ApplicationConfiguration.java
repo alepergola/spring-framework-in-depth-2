40d73167285d069d7086874ffed8188c3f977d28
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-11 00:07:39 +0200

    Moved config in test

--- src/test/java/com/github/alepergola/spring_framework_in_depth_2/config/ApplicationConfiguration.java
+++ src/test/java/com/github/alepergola/spring_framework_in_depth_2/config/ApplicationConfiguration.java
@@ -1,12 +1,11 @@
 package com.github.alepergola.spring_framework_in_depth_2.config;
 
-import com.github.alepergola.spring_framework_in_depth_2.Application;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan(basePackageClasses = Application.class)
+@ComponentScan(basePackages = "com.github.alepergola.spring_framework_in_depth_2")
 @EnableAspectJAutoProxy
 public class ApplicationConfiguration {
 }
