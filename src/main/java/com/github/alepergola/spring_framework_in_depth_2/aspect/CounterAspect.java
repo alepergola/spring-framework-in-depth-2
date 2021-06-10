f76417954e3cc9f40a14442aa999a5d8ceec2b9b
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-10 23:35:39 +0200

    Added counterService in CounterAspect

--- src/main/java/com/github/alepergola/spring_framework_in_depth_2/aspect/CounterAspect.java
+++ src/main/java/com/github/alepergola/spring_framework_in_depth_2/aspect/CounterAspect.java
@@ -1,15 +1,25 @@
 package com.github.alepergola.spring_framework_in_depth_2.aspect;
 
+import com.github.alepergola.spring_framework_in_depth_2.service.CounterService;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class CounterAspect {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final CounterService counterService;
+
+    public CounterAspect(CounterService counterService) {
+        this.counterService = counterService;
+    }
 
     @Pointcut("@annotation(Counter)")
     public void countMethodCall() {
@@ -17,7 +27,10 @@ public class CounterAspect {
 
     @Before("countMethodCall()")
     public void countMethodCall(JoinPoint pointcut) {
-        System.out.println("Hello AspectJ!");
+        String methodName = pointcut.getTarget().toString();
+        int methodCallCount = counterService.count(methodName);
+
+        logger.info("{} : {}", methodName, methodCallCount);
     }
 
 }
